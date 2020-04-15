dd733db5740c564055d902a205dd1f225759335d
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2020-04-15 16:58:55 +0200

    [SE-3] Apply new templates in reimbursement email

---
+++ core/app/helpers/spree/mail_helper.rb
@@ -0,0 +1,10 @@
+module Spree
+  module MailHelper
+    include BaseHelper
+
+    def variant_image_url(variant)
+      image = variant.images.first
+      image ? main_app.url_for(image.url(:small)) : 'noimage/small.png'
+    end
+  end
+end
